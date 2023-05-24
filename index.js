b4baba2899aa5d718a0ac181e9b44d57cfff6a5d
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-05-24 16:24:36 +0500

    main

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then(res => res.json())
-      .then(json => console.log(json))
+fetch("http://localhost:3000/posts")
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach(elem => {
+            let container = document.querySelector(".container")
+            container.innerHTML += `
+            <div class="col-3">
+            <img width="300" src=${elem?.img} alt="">
+            <p>${elem.name}</p> 
+            </div>        
+         `
+        });
+    })
